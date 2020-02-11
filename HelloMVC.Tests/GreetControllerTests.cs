f187a28f32f672a210879267c400f7603cb434a6
Tatyana Bailey <btatyana45@gmail.com>
2020-02-11 14:07:55 -0500

    Added GreetController and it Index view

---
+++ HelloMVC.Tests/GreetControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using HelloMVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Tests
+{
+    public class GreetControllerTests
+    {
+        [Fact]
+        public void Index_Sets_Name_On_Model()
+        {
+            var expectedName = "ExampleString";
+            var controller = new GreetController();
+
+            var result = controller.Index(expectedName);
+
+            var model = (GreetModel)result.Model;
+            Assert.Equal(expectedName, model.Name);
+        }
+
+        [Fact]
+        public void Index_Passes_GreetModel_To_View()
+        {
+            var controller = new GreetController();
+            var result = controller.Index("ThisIsSPAARTAAAA!");
+            Assert.IsType<GreetModel>(result.Model);
+        }
+
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+            var controller = new GreetController();
+
+            var result = controller.Index("ThisIsAString");
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
+
---
+++ HelloMVC/Controllers/GreetController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class GreetController : Controller
+    {
+        public ViewResult Index(string name)
+        {
+            var model = new GreetModel();
+            model.Name = name;
+            return View(model);
+        }
+    }
+}
